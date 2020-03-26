e647e77282eb3ef9538e7ea019954285b4fa49df
Pushpinder Pal Singh <pushpinder@totamail.com>
2020-03-26 15:38:21 +0530

    Add Controller

---
+++ Agora/Controller/ElectionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ElectionViewController.swift
+//  Agora
+//
+//  Created by Pushpinder Pal Singh on 26/03/20.
+//  Copyright © 2020 Pushpinder Pal Singh. All rights reserved.
+//
+
+import UIKit
+
+class ElectionViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
